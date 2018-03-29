59fb8e792bcca123df24557bbd2e88b7100b326d
Mario Perez <mapreal19@gmail.com>
2018-03-29 15:33:54 +0200

    Init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+
+const Office = window.Office;
+
+Office.initialize = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  registerServiceWorker();
+};
