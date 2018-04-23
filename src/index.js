3b4868aa277e36dd497f1c752f18e6b5ea44f0e5
Mario Perez <mapreal19@gmail.com>
2018-04-23 12:17:12 +0200

    Image assets

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import './index.css';
 import 'toastr/build/toastr.min.css';
 import 'purecss/build/pure-min.css';
 
-window.Office.initialize = () => {
+const bootstrap = () =>
   ReactDOM.render(<App />, document.getElementById('root'));
-  registerServiceWorker();
-};
+
+bootstrap();
+registerServiceWorker();
+
+// INFO: The add-in is ready to start interacting with the application and hosted document.
+window.Office.initialize = () => bootstrap();
