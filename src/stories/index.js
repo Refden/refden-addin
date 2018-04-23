0d9f530b2e4afe0ac17c45fa0cb3b596cbba6793
Mario Perez <mapreal19@gmail.com>
2018-04-23 14:05:14 +0200

    storybook-host setup

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { host } from 'storybook-host';
 
 import Login from '../components/Login';
 
 import 'purecss/build/pure-min.css';
 
+const WORD_TASK_PANE_WIDTH = '330px';
+
+const customHost = () => host({
+  width: WORD_TASK_PANE_WIDTH,
+  height: '100%',
+  border: true,
+});
+
 storiesOf('Login', module)
+  .addDecorator(customHost())
   .add('default', () => <Login handleLogin={action('Login...')} />);
