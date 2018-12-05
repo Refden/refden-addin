4d4bd98e4f4b0726108c6bafe288974063ab83c1
Mario Perez <mapreal19@gmail.com>
2018-12-05 13:18:07 +0100

    Removes service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 import 'toastr/build/toastr.min.css';
@@ -12,9 +11,6 @@ import 'purecss/build/pure-min.css';
 const bootstrap = () =>
   initializeIcons();
   ReactDOM.render(<App />, document.getElementById('root'));
-
-bootstrap();
-registerServiceWorker();
 
 // INFO: The add-in is ready to start interacting with the application and hosted document.
 window.Office.initialize = () => bootstrap();
