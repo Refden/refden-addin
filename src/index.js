bd7ce698d0af8eeb7090972a763f1770df3b468f
Mario Perez <mapreal19@gmail.com>
2021-01-18 11:57:05 +0100

    chore(logrocket): code for tracking

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 import 'react-app-polyfill/ie11'; // NOTE: support for ie11 for now
+import LogRocket from 'logrocket';
 
 import App from './components/App';
 
@@ -13,6 +14,10 @@ import 'purecss/build/pure-min.css';
 
 // INFO: The add-in is ready to start interacting with the application and hosted document.
 window.Office.initialize = () => {
+  if (process.env.NODE_ENV !== 'development') {
+    LogRocket.init('dfid9b/refden-word');
+  }
+
   initializeIcons();
   ReactDOM.render(<App />, document.getElementById('root'));
 };
