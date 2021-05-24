d3615264a98703b3680c2430bee676fb90672246
Mario Perez <mapreal19@gmail.com>
2021-05-24 17:26:12 +0200

    refactor: settings component change to func component.

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import _ from 'lodash/fp';
-import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
+import { Dropdown, DropdownMenuItemType } from 'office-ui-fabric-react/lib/Dropdown';
 
 import Bibliography from '../Bibliography/Bibliography';
 import {
@@ -10,6 +10,13 @@ import { updateBibliography } from '../../lib/bibliography/index';
 
 const DEFAULT_STYLE = 'apa';
 const DEFAULT_LOCALE = 'en-US';
+
+const dropDownStyles = [
+  { key: 'Header', text: 'Popular styles', itemType: DropdownMenuItemType.Header },
+  ...STYLES,
+  { key: 'divider_1', text: '-', itemType: DropdownMenuItemType.Divider },
+  { key: 'Header2', text: 'Other styles', itemType: DropdownMenuItemType.Header },
+];
 
 const initializeLocalStorage = () => {
   if (_.isEmpty(localStorage.getItem(LOCAL_STORAGE__STYLE))) {
@@ -21,53 +28,52 @@ const initializeLocalStorage = () => {
   }
 };
 
-class Settings extends Component {
-  constructor(props) {
-    super(props);
+const Settings = () => {
+  const [style, setStyle] = useState('');
+  const [locale, setLocale] = useState('');
 
+  useEffect(() => {
     initializeLocalStorage();
 
-    this.state = {
-      selectedStyleKey: localStorage.getItem(LOCAL_STORAGE__STYLE),
-      selectedLocaleKey: localStorage.getItem(LOCAL_STORAGE__LOCALE),
-    };
-  }
+    setStyle(localStorage.getItem(LOCAL_STORAGE__STYLE));
+    setLocale(localStorage.getItem(LOCAL_STORAGE__LOCALE));
+  }, []);
 
-  changeStyle = (_event, item) => {
+  const changeStyle = (_event, item) => {
     localStorage.setItem(LOCAL_STORAGE__STYLE, item.key);
-    this.setState({ selectedStyleKey: item.key });
-    updateBibliography();
+    setStyle(item.key);
+    updateBibliography(); // TODO useEffect?
   };
 
-  changeLocale = (_event, item) => {
+  const changeLocale = (_event, item) => {
     localStorage.setItem(LOCAL_STORAGE__LOCALE, item.key);
-    this.setState({ selectedLocaleKey: item.key });
+    setLocale(item.key);
     updateBibliography();
   };
 
-  render = () => (
-    [
+  return (
+    <>
       <div key="styles" className="pure-u-1">
         <Dropdown
           label="Style:"
-          selectedKey={this.state.selectedStyleKey}
-          onChange={this.changeStyle}
+          selectedKey={style}
+          onChange={changeStyle}
           placeholder="Select an Style"
-          options={STYLES}
+          options={dropDownStyles}
         />
-      </div>,
+      </div>
       <div key="locales" className="pure-u-1">
         <Dropdown
           label="Locale:"
-          selectedKey={this.state.selectedLocaleKey}
-          onChange={this.changeLocale}
+          selectedKey={locale}
+          onChange={changeLocale}
           placeholder="Select a Locale"
           options={LOCALES}
         />
-      </div>,
-      <Bibliography key="bibliography" />,
-    ]
+      </div>
+      <Bibliography key="bibliography" />
+    </>
   );
-}
+};
 
 export default Settings;
