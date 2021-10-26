bc3218e664ffb9fc46902fead0c859c6563d3b34
Juan Iscar <juan.iscar@exponentiateam.com>
2021-10-26 11:57:48 +0200

    Delete JSON Signed-off-by: Juan Iscar <juan.iscar@exponentiateam.com>

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -2,12 +2,11 @@ import localForage from 'localforage';
 
 export const authHeaders = () => {
   const promise = localForage.getItem('auth-headers');
-
-  return Promise.resolve(promise).then((result) => JSON.parse(result));
+  return Promise.resolve(promise).then((result) => result);
 };
 
 export const setAuthHeaders = (headers) => {
-  localForage.setItem('auth-headers', JSON.stringify(headers));
+  localForage.setItem('auth-headers', headers);
 };
 
 export const removeAuthHeaders = () => {
